9b68c0ab587488fb392c20374a17325ec23d8995
ALIRAZA <alirazamikraam@gmail.com>
2024-12-18 18:04:26 +0500

    feat: add google link in header for seo purpose

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Home from "./pages/LandingPage";
 
 function App() {
   useEffect(() => {
-    // Add the Meta Pixel script to the head
-    const script = document.createElement("script");
-    script.async = true;
-    script.innerHTML = `
+    // Add the Meta Pixel script
+    const metaPixelScript = document.createElement("script");
+    metaPixelScript.async = true;
+    metaPixelScript.innerHTML = `
       !function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
       n.queue=[];t=b.createElement(e);t.async=!0;
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
@@ -18,20 +18,38 @@ function App() {
       fbq('init', '764407359216880');
       fbq('track', 'PageView');
     `;
-    document.head.appendChild(script);
+    document.head.appendChild(metaPixelScript);
 
-    // Add the noscript tag for fallback
-    const noscript = document.createElement("noscript");
-    noscript.innerHTML = `
+    // Add the noscript tag for Meta Pixel fallback
+    const metaPixelNoscript = document.createElement("noscript");
+    metaPixelNoscript.innerHTML = `
       <img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=764407359216880&ev=PageView&noscript=1"/>
     `;
-    document.body.appendChild(noscript);
+    document.body.appendChild(metaPixelNoscript);
+
+    // Add the Google tag script
+    const googleTagScript = document.createElement("script");
+    googleTagScript.async = true;
+    googleTagScript.src =
+      "https://www.googletagmanager.com/gtag/js?id=AW-16476282520";
+    document.head.appendChild(googleTagScript);
+
+    const googleTagInit = document.createElement("script");
+    googleTagInit.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-16476282520');
+    `;
+    document.head.appendChild(googleTagInit);
 
     // Cleanup on unmount
     return () => {
-      document.head.removeChild(script);
-      document.body.removeChild(noscript);
+      document.head.removeChild(metaPixelScript);
+      document.body.removeChild(metaPixelNoscript);
+      document.head.removeChild(googleTagScript);
+      document.head.removeChild(googleTagInit);
     };
   }, []);
 
